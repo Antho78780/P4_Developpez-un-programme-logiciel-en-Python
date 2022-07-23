c3c32147ae549eb21873282191a4fee2cb1c8eca
Anthony Audrain <anthonyaud78@gmail.com>
2022-07-23 02:44:19 +0200

    ajout la méthode pour triez les joueurs par classement au premier round

--- controller.py
+++ controller.py
@@ -15,7 +15,6 @@ class Controller:
         self.tournaments_table = self.db.table("tournaments")
 
     """Créer un tournoi"""
-
     def create_tournament(self):
         tournaments = self.tournaments(
             self.view.prompt_name_tournament(self.menu_tournament),
@@ -24,8 +23,9 @@ class Controller:
             self.view.prompt_time_tournament(self.menu_tournament),
         )
         print(tournaments)
-        self.view.get_players_tournaments_database(self.get_players_tournament_database)
+        self.view.get_players_tournaments_database(self.get_players_database)
         self.view.prompt_add_player(self.Query, self.players_table, tournaments.add_player)
+        tournaments.rounds = []
         tournaments.number_round = 0
         serialized_tournament = {
             "nom": tournaments.name,
@@ -33,6 +33,7 @@ class Controller:
             "date": tournaments.date,
             "temps": tournaments.time,
             "number_round": tournaments.number_round,
+            "rounds": tournaments.rounds,
             "joueurs": tournaments.add_player
         }
         self.tournaments_table.insert(serialized_tournament)
@@ -40,8 +41,7 @@ class Controller:
         self.return_menu()
 
     """Ajout de joueur"""
-
-    def add_player(self):
+    def create_player(self):
         players = self.players(
             self.view.prompt_userName_player(self.menu_player),
             self.view.prompt_name_player(self.menu_player),
@@ -59,10 +59,12 @@ class Controller:
         }
         self.players_table.insert(player_serialized)
         self.view.phrasing_create_player()
-        self.return_menu()
+        if self.return_menu() == "n" or "N":
+            self.create_player()
+        else:
+            self.return_menu()
 
     """Informations des joueurs dans la base de donnée"""
-
     def get_players_database(self):
         if self.players_table.all() == []:
             self.view.phrasing_none_players()
@@ -74,8 +76,10 @@ class Controller:
                                       date_birth=player["date_de_naissance"],
                                       sex=player["sexe"],
                                       ranking=player["classement"])
+
                 print(player)
             self.view.phrasing_len_players(self.players_table.all)
+
             self.return_menu()
 
     """Informations des tournois dans la base de donnée"""
@@ -92,20 +96,18 @@ class Controller:
                     date=tournament["date"],
                     time=tournament["temps"]
                 )
+                self.delete_date_birth_sex(tournament["joueurs"])
                 tr.add_player = tournament["joueurs"]
                 print(tr)
             self.return_menu()
 
-    """Information des joueurs qui sont dans la base des donnée dans les tournois"""
-
-    def get_players_tournament_database(self):
-        if len(self.players_table.all()) >= 8:
-            for player in self.players_table.all():
-                print(player)
-            self.view.phrasing_len_players(self.players_table.all)
-        else:
-            self.view.phrasing_none_players_tournament(self.players_table.all)
-            self.return_menu()
+    def delete_date_birth_sex(self, tournament_joueurs):
+        for trt in tournament_joueurs:
+            try:
+                trt.pop("date_de_naissance")
+                trt.pop("sexe")
+            except:
+                return KeyError
 
     def create_round(self):
         trt = self.Query()
@@ -115,68 +117,66 @@ class Controller:
             }
             print(objetTr)
         search_name_tournament = self.tournaments_table.search(trt.nom == self.view.prompt_phrasing_name_tournament())
-        if search_name_tournament:
-            for tournament in self.tournaments_table.all():
-                if tournament["nom"] == search_name_tournament[0]["nom"]:
-                    if tournament["number_round"] <= 1:
-                        self.tournaments_table.update(
-                            {"number_round": self.view.prompt_nRound()}, trt.nom == search_name_tournament[0]["nom"]
-                        )
-                        search_name_tournament[0]["number_round"] = tournament["number_round"]
-                        if search_name_tournament[0]["number_round"] <= 1:
-                            for tr in search_name_tournament[0]["joueurs"]:
-                                try:
-                                    tr.pop("date_de_naissance")
-                                    tr.pop("sexe")
-                                except:
-                                    return KeyError
-
-                            sup_moitie = search_name_tournament[0]["joueurs"][4:]
-                            inf_moitie = search_name_tournament[0]["joueurs"][:4]
-                            sup_moitie.sort(key=lambda x: x.get("classement"))
-                            inf_moitie.sort(key=lambda x: x.get("classement"))
-                            print("Les joueurs on été triés par classement")
-                            for sup in sup_moitie:
-                                print(sup)
-
-                            print("-----------------------------------------------------------")
-                            for inf in inf_moitie:
-                                print(inf)
-
-                            print("match1: ", sup_moitie[0]["prenom"], "vs", inf_moitie[0]["prenom"])
-                            print("match2: ", sup_moitie[1]["prenom"], "vs", inf_moitie[1]["prenom"])
-                            print("match3: ", sup_moitie[2]["prenom"], "vs", inf_moitie[2]["prenom"])
-                            print("match4: ", sup_moitie[3]["prenom"], "vs", inf_moitie[3]["prenom"])
-                            self.return_menu()
-                    else:
-                        print("il y a déja 1 round qui a été créer sur ce tournoi")
-                        self.return_menu()
+        if search_name_tournament and search_name_tournament[0]["number_round"] == 0:
+            self.tournaments_table.update(
+                {"number_round": self.view.prompt_nRound()}, trt.nom == search_name_tournament[0]["nom"]
+            )
+            self.triePlayerRound(search_name_tournament, self.tournaments_table)
         else:
             print("Tournoi introuvable")
             self.return_menu()
 
-    """Retour au menu"""
+    """Trie des joueurs au premier round"""
+    def triePlayerRound(self, search_name_tournament, tournament_table):
+        sup_moitie = search_name_tournament[0]["joueurs"][4:]
+        inf_moitie = search_name_tournament[0]["joueurs"][:4]
+        sup_moitie.sort(key=lambda x: x.get("classement"))
+        inf_moitie.sort(key=lambda x: x.get("classement"))
+        print("------------Joueurs-rangés-par-classement------------------")
+        for sup in sup_moitie:
+            print(sup)
+        for inf in inf_moitie:
+            print(inf)
+        print("---------------------------Round1-------------------------------")
+        match1 = ([sup_moitie[0]["prenom"], "score"], [inf_moitie[0]["prenom"], "score"])
+        match2 = ([sup_moitie[1]["prenom"], "score"], [inf_moitie[1]["prenom"], "score"])
+        match3 = ([sup_moitie[2]["prenom"], "score"], [inf_moitie[2]["prenom"], "score"])
+        match4 = ([sup_moitie[3]["prenom"], "score"], [inf_moitie[3]["prenom"], "score"])
+        print(match1)
+        print(match2)
+        print(match3)
+        print(match4)
+        for i in match1, match2, match3, match4:
+            print("----------------------Score-Match-------------------------")
+            print("Match: ", i)
+            result_match_first_players = int(input(f"resultat du match pour {i[0]}: "))
+            i[0][1] = result_match_first_players
+            result_match_last_players = int(input(f"resultat du match pour {i[1]}: "))
+            i[1][1] = result_match_last_players
+        tournament_table.update(
+            {"rounds": match1 + match2 + match3 + match4}
+        )
+        print("----------------------Résultat-Match--------------------------")
+        print(match1, match2, match3, match4)
 
+
+    """Retour au menu"""
     def return_menu(self):
         self.view.return_menu(self.menu)
 
     """Visualisation du menu player"""
-
     def menu_player(self):
-        self.view.menu_player(self.add_player, self.get_players_database, self.menu)
+        self.view.menu_player(self.create_player, self.get_players_database, self.menu)
 
     """Visualisation du menu Tournoi"""
-
     def menu_tournament(self):
         self.view.menu_tournament(self.create_tournament, self.create_round, self.get_tournaments_database,
                                   self.menu)
 
     """Visualisation du menu général"""
-
     def menu(self):
         self.view.menu(self.menu_player, self.menu_tournament)
 
     """Lancer le code"""
-
     def run(self):
         self.menu()
