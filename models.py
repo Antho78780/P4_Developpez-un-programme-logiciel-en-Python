20391643835f1ab10a7b686fbad67ba48a6c2048
Anthony Audrain <anthonyaud78@gmail.com>
2022-07-24 16:06:59 +0200

    remplacement d'attibut

--- models.py
+++ models.py
@@ -36,16 +36,18 @@ class Tournaments:
 
 
 class Rounds:
-    def __init__(self, name, heure_round, date_round):
+    def __init__(self, name, heure_start, date_start):
         self.name = name
-        self.list_match = []
-        self.date_round = date_round
-        self.heure_round = heure_round
+        self.heure_start = heure_start
+        self.date_start = date_start
+        self.date_end = ""
+        self.heure_end = ""
+        self.matchs = []
 
     def __str__(self):
-        round_presentation = f"nom: {self.name}, list_match: {self.list_match}, date_round: {self.date_round}, temps_round: {self.heure_round},"
+        round_presentation = f"nom: {self.name},heure_debut: {self.heure_start},date_debut: {self.date_start}," \
+                             f"heure_fin:{self.heure_end}, date_fin: {self.date_end},  matchs: {self.matchs}"
         return round_presentation
-
 
 
 
